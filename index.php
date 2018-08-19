bc125d8244a5d2b844058b4541ad40fe53c085f1
mdmmm100 <42524356+mdmmm100@users.noreply.github.com>
2018-08-20 07:59:23 +0800

    Added index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Palaski Innovation</title>
+</head>
+<body>
+	<h1>This is the website</h1>
+	<?php 
+		for ($i=0; $i < 5; $i++) { 
+			echo "<h" . i . ">This size is " . i . "</h" . i . ">";
+		}
+	 ?>
+</body>
+</html>
